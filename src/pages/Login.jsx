152f5913073de8373a05b47ba6917fb14f221aca
CharlesCypher <99364836+CharlesCypher@users.noreply.github.com>
2023-06-01 21:34:39 +0100

    updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import { firebaseAuth } from "../utils/firebase-config";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Navlogin from "../components/Navlogin";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
@@ -41,24 +41,29 @@ function Login() {
   return (
     <>
       {/* <Navlogin /> */}
-      <Header />
+      {/* <Header /> */}
       <div className="login">
         <div className="login__left">
           <div className="img__container">
             <img
-              src="https://images.unsplash.com/photo-1684248182033-3777c303f726?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIzfHhqUFI0aGxrQkdBfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+              src="https://images.unsplash.com/photo-1683803168320-ab1162844e89?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDM1fHhqUFI0aGxrQkdBfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
               alt=""
             />
           </div>
+          <div className="overlay"></div>
         </div>
         <div className="login__right">
           <form className="form" onSubmit={handleSubmit(handleLogin)}>
-            <h1 className="form__title">Login</h1>
-            <input type="email" name="email" placeholder="Email Address" required {...register("email")} />
-            <p>{errors.email?.message}</p>
+            <h1 className="form__title">Welcome</h1>
+            <input type="email" name="email" placeholder="Email" {...register("email")} />
+            <p className="errors">{errors.email?.message}</p>
             <input type="password" name="password" placeholder="Password" {...register("password")} />
-            <p>{errors.password?.message}</p>
-            <input className="login__btn" type="submit" value="login" />
+            <p className="errors">{errors.password?.message}</p>
+            <input className="login__btn" type="submit" value="login" aria-label="login" />
+            <p className="alt">
+              Don't have an account?
+              <Link to={"/register"}>Register</Link>
+            </p>
           </form>
         </div>
       </div>
