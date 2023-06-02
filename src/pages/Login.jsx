628c554f93be4304fd45fb54475155e1159337bc
CharlesCypher <99364836+CharlesCypher@users.noreply.github.com>
2023-06-02 02:32:25 +0100

    updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,12 +2,10 @@ import React, { useEffect, useState } from "react";
 import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import { firebaseAuth } from "../utils/firebase-config";
 import { Link, useNavigate } from "react-router-dom";
-import Navlogin from "../components/Navlogin";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import "./Login.css";
-import Header from "../components/Header";
+import styles from "./form.module.css";
 
 function Login() {
   const schema = yup.object().shape({
@@ -40,27 +38,26 @@ function Login() {
 
   return (
     <>
-      {/* <Navlogin /> */}
-      {/* <Header /> */}
-      <div className="login">
-        <div className="login__left">
-          <div className="img__container">
+      <div className={styles.login}>
+        <div className={styles.login__left}>
+          <div className={styles.img__container}>
             <img
+              className={styles.img}
               src="https://images.unsplash.com/photo-1683803168320-ab1162844e89?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDM1fHhqUFI0aGxrQkdBfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
               alt=""
             />
           </div>
-          <div className="overlay"></div>
+          <div className={styles.overlay}></div>
         </div>
-        <div className="login__right">
-          <form className="form" onSubmit={handleSubmit(handleLogin)}>
-            <h1 className="form__title">Welcome</h1>
+        <div className={styles.login__right}>
+          <form className={styles.form} onSubmit={handleSubmit(handleLogin)}>
+            <h1 className={styles.form__title}>Welcome</h1>
             <input type="email" name="email" placeholder="Email" {...register("email")} />
-            <p className="errors">{errors.email?.message}</p>
+            <p className={styles.errors}>{errors.email?.message}</p>
             <input type="password" name="password" placeholder="Password" {...register("password")} />
-            <p className="errors">{errors.password?.message}</p>
-            <input className="login__btn" type="submit" value="login" aria-label="login" />
-            <p className="alt">
+            <p className={styles.errors}>{errors.password?.message}</p>
+            <input className={styles.login__btn} type="submit" value="login" aria-label="login" />
+            <p className={styles.alt}>
               Don't have an account?
               <Link to={"/register"}>Register</Link>
             </p>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { firebaseAuth } from "../utils/firebase-config";
-import Navlogin from "../components/Navlogin";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import styles from "./form.module.css";
 
 function Signup() {
   const schema = yup.object().shape({
@@ -35,17 +35,34 @@ function Signup() {
   }, []);
 
   return (
-    <div>
-      <Navlogin login={"isLogged"} />
-      <form onSubmit={handleSubmit(handleSignIn)}>
-        <h1 className="form__title">Register</h1>
-        <input type="email" name="email" placeholder="Email Address" {...register("email")} />
-        <span>{errors.email?.message}</span>
-        <input type="password" name="password" placeholder="Password" {...register("password")} />
-        <span>{errors.password?.message}</span>
-        <button type="submit">Register</button>
-      </form>
-    </div>
+    <>
+      <div className={styles.login}>
+        <div className={styles.login__left}>
+          <div className={styles.img__container}>
+            <img
+              className={styles.img}
+              src="https://images.unsplash.com/photo-1683803168320-ab1162844e89?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDM1fHhqUFI0aGxrQkdBfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+              alt=""
+            />
+          </div>
+          <div className={styles.overlay}></div>
+        </div>
+        <div className={styles.login__right}>
+          <form className={styles.form} onSubmit={handleSubmit(handleSignIn)}>
+            <h1 className={styles.form__title}>Register</h1>
+            <input type="email" name="email" placeholder="Email" {...register("email")} />
+            <p className={styles.errors}>{errors.email?.message}</p>
+            <input type="password" name="password" placeholder="Password" {...register("password")} />
+            <p className={styles.errors}>{errors.password?.message}</p>
+            <input className={styles.login__btn} type="submit" value="Sign up" aria-label="sign up" />
+            <p className={styles.alt}>
+              Already have an account?
+              <Link to={"/login"}>Login</Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
 
